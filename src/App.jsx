1a5af6e7ec1d0b7129b5de5bf9405159ab4fa03b
andymedina1 <107598069+andymedina1@users.noreply.github.com>
2024-10-28 20:20:43 -0300

    feat: added first TodoItem function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import './App.css'
 
 function App () {
-  return (<h1>Hello World!</h1>)
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+    </>
+  )
+}
+
+function TodoItem () {
+  return (
+    <li>
+      <span>V</span>
+      <p>Llorar con la Llorona</p>
+      <span>X</span>
+    </li>
+  )
 }
 
 export default App
