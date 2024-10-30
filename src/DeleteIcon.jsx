45ea4f7c3fc412ba9f656aefa0286dd742b9e2ef
andymedina1 <107598069+andymedina1@users.noreply.github.com>
2024-10-29 22:09:56 -0300

    feat: added complete and delete icons

---
+++ src/DeleteIcon.jsx
@@ -0,0 +1,11 @@
+import { TodoIcon } from './TodoIcon'
+
+export function DeleteIcon ({ onDelete }) {
+  return (
+    <TodoIcon
+      type='delete'
+      color='gray'
+      onClick={onDelete}
+    />
+  )
+}
--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,25 +1,24 @@
+import { CompleteIcon } from './CompleteIcon'
+import { DeleteIcon } from './DeleteIcon'
 import './TodoItem.css'
 
 export function TodoItem ({ text, completed, onComplete, onDelete }) {
   return (
     <li className='TodoItem'>
-      <span
-        className={`Icon Icon-check ${completed ? 'Icon-check--active' : ''}`}
-        onClick={onComplete}
-      >
-        V {completed}
-      </span>
+      <CompleteIcon
+        completed={completed}
+        onComplete={onComplete}
+      />
+
       <p
         className={`TodoItem-p ${completed ? 'TodoItem-p--complete' : ''}`}
       >
         {text}
       </p>
-      <span
-        className='Icon Icon-delete'
-        onClick={onDelete}
-      >
-        X
-      </span>
+
+      <DeleteIcon
+        onDelete={onDelete}
+      />
     </li>
   )
 }
